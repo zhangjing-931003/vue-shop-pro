c1ed53a6a4b28b7df039a243703427c41477fb50
zhangjing-vue <403278438@qq.com>
2019-02-14 23:16:12 +0800

    登录功能开发完毕

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入css样式全局控制
+import './assets/css/global.css'
+// 引入element-ui
+import ElementUI from 'element-ui'
+// 引入图标样式
+import './assets/fonts/iconfont.css'
+// 引入axios
+import axios from 'axios'
+// 给axios做配置（访问根地址）
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+// 给vue注册全部的element-ui的标签组件和事件方法组件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
