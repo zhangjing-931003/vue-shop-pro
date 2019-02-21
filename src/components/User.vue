ac8d0be77d37880766ab37749ba269a177635997
zhangjing-vue <403278438@qq.com>
2019-02-21 16:28:18 +0800

    用户管理开发完毕

--- src/components/User.vue
+++ src/components/User.vue
@@ -6,8 +6,8 @@
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card class="box-card">
-      <el-row gutter="20">
-        <el-col span="8">
+      <el-row :gutter="20">
+        <el-col :span="8">
           <el-input
             placeholder="请输入搜索内容"
             v-model="queryParams.query"
@@ -18,10 +18,52 @@
             <el-button slot="append" icon="el-icon-search" @click="getUserInfos"></el-button>
           </el-input>
         </el-col>
-        <el-col span="6">
-          <el-button type="primary">添加用户</el-button>
+        <el-col :span="6">
+          <el-button type="primary" @click="addDialogVisible=true">添加用户</el-button>
         </el-col>
       </el-row>
+      <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%" @close="addDialogClose">
+        <el-form :rules="addFormRules" ref="addFormRef" :model="addForm" label-width="80px">
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="addForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="addForm.password"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="addForm.email"></el-input>
+          </el-form-item>
+          <el-form-item label="手机号码" prop="mobile">
+            <el-input v-model="addForm.mobile"></el-input>
+          </el-form-item>
+        </el-form>
+        <span>
+          <el-button @click="addDialogVisible=false">取消</el-button>
+          <el-button type="primary" @click="addUser">确定</el-button>
+        </span>
+      </el-dialog>
+      <el-dialog
+        title="修改用户"
+        :visible.sync="editDialogVisible"
+        width="50%"
+        @close="editDialogClose"
+      >
+        <el-form :rules="editFormRules" ref="editFormRef" :model="editForm" label-width="80px">
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="editForm.username" :disabled="true"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="editForm.email"></el-input>
+          </el-form-item>
+          <el-form-item label="手机号码" prop="mobile">
+            <el-input v-model="editForm.mobile"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="addDialogVisible=false">取消</el-button>
+          <el-button type="primary" @click="editUser">确定</el-button>
+        </span>
+      </el-dialog>
       <el-table :data="userInfos" border style="width: 100%">
         <el-table-column type="index" label="序号" width="60"></el-table-column>
         <el-table-column prop="username" label="用户名"></el-table-column>
@@ -31,22 +73,34 @@
         <el-table-column label="状态" width="180">
           <template slot-scope="scope">
             <el-switch
-              v-model= "scope.row.mg_state"
-              @change= "changeState(scope.row.id,scope.row.mg_state)"
+              v-model="scope.row.mg_state"
+              @change="changeState(scope.row.id,scope.row.mg_state)"
             ></el-switch>
           </template>
         </el-table-column>
         <el-table-column label="操作" width="230">
-          <el-button type="primary" icon="el-icon-edit" sizi="mini"></el-button>
-          <el-button type="danger" icon="el-icon-delete" sizi="mini"></el-button>
-          <el-tooltip content="分配角色" placement="top" :enterable="false">
-            <el-button type="warning" icon="el-icon-setting" sizi="mini"></el-button>
-          </el-tooltip>
+          <template slot-scope="info">
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+              @click="showEditDialog(info.row.id)"
+            ></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+              @click="delUser(info.row.id)"
+            ></el-button>
+            <el-tooltip content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
         </el-table-column>
       </el-table>
       <el-pagination
         @size-change="handleSizeChange"
-        @cuttrent-change="handleCurrentChange"
+        @current-change="handleCurrentChange"
         :current-page="queryParams.pagenum"
         :page-sizes="[3,5,10,20]"
         :page-size="queryParams.pagesize"
@@ -62,6 +116,68 @@ export default {
     this.getUserInfos()
   },
   methods: {
+    async showEditDialog(id) {
+      this.editDialogVisible = true
+      const { data: res } = await this.$http.get('users/' + id)
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      this.editForm = res.data
+    },
+    editDialogClose() {
+      this.$refs.editFormRef.resetFields()
+    },
+    editUser() {
+      this.$refs.editFormRef.validate(async valid => {
+        if (valid) {
+          const { data: res } = await this.$http.put(
+            'users/' + this.editForm.id,
+            this.editForm
+          )
+          if (res.meta.status !== 200) {
+            return this.$message.error(res.meta.msg)
+          }
+          this.editDialogVisible = false
+          this.$message.success(res.meta.msg)
+          this.getUserInfos()
+        }
+      })
+    },
+    delUser(id) {
+      this.$confirm('确定要删除该会员吗？', '删除', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(async() => {
+          const { data: res } = await this.$http.delete('users/' + id)
+          if (res.meta.status !== 200) {
+            return this.$message.error(res.meta.msg)
+          }
+          this.$message.success(res.meta.msg)
+          if (this.userInfos.length === 1 && this.queryParams.pagenum > 1) {
+            this.queryParams.pagenum--
+          }
+          this.getUserInfos()
+        })
+        .catch(() => {})
+    },
+    addUser() {
+      this.$refs.addFormRef.validate(async valid => {
+        if (valid) {
+          const { data: res } = await this.$http.post('users', this.addForm)
+          if (res.meta.status !== 201) {
+            return this.$message.error(res.meta.msg)
+          }
+          this.addDialogVisible = false
+          this.$message.success(res.meta.msg)
+          this.getUserInfos()
+        }
+      })
+    },
+    addDialogClose() {
+      this.$refs.addFormRef.resetFields()
+    },
     async changeState(id, state) {
       const { data: res } = await this.$http.put(`users/${id}/state/${state}`)
       if (res.meta.status !== 200) {
@@ -93,7 +209,25 @@ export default {
     }
   },
   data() {
+    var checkMobile = (rule, value, callback) => {
+      if (!/^1[35789]\d{9}$/.test(value)) {
+        return callback(new Error('手机号码格式不正确'))
+      }
+      callback()
+    }
     return {
+      editDialogVisible: false,
+      editForm: {
+        username: '',
+        email: '',
+        mobile: ''
+      },
+      editFormRules: {
+        mobile: [
+          { required: true, message: '手机号码必填', trigger: 'blur' },
+          { validator: checkMobile, trigger: blur }
+        ]
+      },
       keywords: '',
       userInfos: [],
       queryParams: {
@@ -101,6 +235,21 @@ export default {
         pagenum: 1,
         pagesize: 3,
         total: 0
+      },
+      addDialogVisible: false,
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      addFormRules: {
+        username: [{ required: true, message: '用户名必填', trigger: 'blur' }],
+        password: [{ required: true, message: '密码必填', trigger: 'blur' }],
+        mobile: [
+          { required: true, message: '手机号码必填', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ]
       }
     }
   }
